c0d7b1fc4a16f895cd88ca9d5425a66cfd52ff91
rsmelo <rodrigosm87@gmail.com>
2019-08-30 19:16:19 -0300

    Profile: add component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+
+const Profile = () => (
+  <StaticQuery
+    query={graphql`
+      query MySiteMetadata {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+            position
+          }
+        }
+      }
+    `}
+    render={({
+      site: {
+        siteMetadata: {
+          title,
+          position,
+          description,
+        }
+      }
+    }) => (
+      <div>
+        <h1>{title}</h1>
+        <h2>{position}</h2>
+        <p>{description}</p>
+    </div>
+    )}
+  />
+)
+
+export default Profile
